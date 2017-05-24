4cffedcd92cbbef36c7b84fea45fec2b853844de
Icey <os_rover@sina.com>
2017-05-24 20:38:59 +0800

    提交学员管理代码

--- SportXRN/js/page/AddGuysFragment.js
+++ SportXRN/js/page/AddGuysFragment.js
@@ -13,13 +13,11 @@ import SearchBar from '../component/SearchBar';
 import Swiper from 'react-native-swiper';
 import UserListView from '../component/UserListView';
 
-export default class GuysFragment extends Component{
+export default class AddGuysFragment extends Component{
   constructor(props){
       super(props);
       this.state = {
-          refreshing: true,
-          loadedData: false,
-          dataBlob: []
+          
       }
   }
 
@@ -48,54 +46,6 @@ export default class GuysFragment extends Component{
               </ActionButton>
           </View>
       );
-  }
-
-  _onRefresh() {
-      this.setState({refreshing: true});
-      this._fetchData();
-  }
-
-  _searchButtonCallback(){
-
-  }
-
-  _renderListView(){
-      if(!this.state.refreshing || this.state.loadedData) {
-          return (
-              <UserListView isRenderHeader={false} contents={this.state.dataBlob} />
-          );
-      }
-  }
-
-  _fetchData(){
-      fetch('http://gold.xitu.io/api/v1/hot/57fa525a0e3dd90057c1e04d/android')
-          .then((response) => response.json())
-          .then((responseData) => {
-              let entry = responseData.rows;
-              var dataBlob = [];
-
-              for(let i in entry){
-                  let itemInfo = {
-                      userName: entry[i].userName, // 用户名
-                      userCode: entry[i].userCode, // 用户编号
-                      excDays: entry[i].excDays ? entry[i].excDays : 0, // 训练天数
-                      latelyDate: entry[i].latelyDate ? entry[i].latelyDate : 0, // 最近训练日期
-                      portrait: entry[i].portrait ?  entry[i].portrait : null // 头像
-                  }
-                  dataBlob.push(itemInfo);
-              }
-
-              this.setState({
-                  dataBlob: dataBlob,
-                  loadedData: true,
-                  refreshing: false
-              });
-          }).done();
-
-  }
-
-  componentDidMount(){
-      this._fetchData();
   }
 }
 
